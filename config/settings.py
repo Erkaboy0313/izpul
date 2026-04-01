df70cde1910d9e331c986879d007525c4668ec41
Erkaboy0313 <fayyozbekov20@gmail.com>
2026-04-01 21:37:34 +0500

    deg

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^ms-_r4i3=p$$)xit4d&76vzo9!h*pjd(7oum-&oo8xx2#0g#3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','izpul.uz']
 
